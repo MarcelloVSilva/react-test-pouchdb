37ef14b9ee80b2007bad5bb64147a8e6f31c07dc
MarcelloVSilva <marcellovcs@gmail.com>
2018-12-12 11:17:30 -0200

    replicando com o banco do matheus

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import PouchDB from 'pouchdb'
 
+
+const db = new PouchDB('hoda5');
+const remote = 'http://localhost:9000/todos'
+const remoteMatheus = 'http://192.168.15.12:9000/teste'
 class App extends Component {
+  constructor() {
+    super()
+    this.add = this.add.bind(this)
+  }
+  componentDidMount() {
+    sync()
+  }
+
+  add() {
+    const date = new Date().toString()
+    const data = {
+      _id: date,
+      descricao: date,
+    }
+    db.put(data).then((res) => {
+      console.log(res)
+    }).catch((err) => console.log(err))
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <button onClick={this.add}>Pouch</button>
       </div>
     );
   }
 }
 
 export default App;
+
+
+var opts = { live: true }
+function sync() {
+  setInterval(() => {
+    db.sync(remote, opts)
+    PouchDB.replicate(remote, remoteMatheus, opts)
+  }, 5000)
+}
